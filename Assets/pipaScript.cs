e234d8b5aabc0d54b9dc7d309db067528ee6e3c0
Revaldo <revaldopratamaputra@Revaldos-MacBook-Pro.local>
2021-01-15 21:12:27 +0700

    UAS Prak.SIsmul A

---
+++ Assets/pipaScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipaScript : MonoBehaviour
+{
+    float nilaiRandom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nilaiRandom = Random.Range(-3.41f, 2.56f);
+        transform.position = new Vector2(transform.position.x, nilaiRandom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gerak ke kiri
+        transform.position = Vector2.MoveTowards(transform.position,Vector2.left * 100, Time.deltaTime * 2);
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMovement : MonoBehaviour
+{
+    Rigidbody2D Rb;
+    public float lompat;
+    //buat socre
+    float score;
+    public Text scoreTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.text = "score : " + score;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rb.velocity = Vector2.up * lompat;
+        }
+    }   
+
+    //buat score
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="point")
+        {
+            score++;
+        }
+
+        if (collision.gameObject.tag=="pipa")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
